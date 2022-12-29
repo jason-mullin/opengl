4b1328a2a4440c2d78567aa9d3950b77934a82f6
jason.mullin.7@gmail.com <jason.mullin.7@gmail.com>
2022-12-28 20:48:26 -0500

    update text

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
  
-    GLFWwindow* window = glfwCreateWindow(800, 600, "this is NOT a window!", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Test Window", NULL, NULL);
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
